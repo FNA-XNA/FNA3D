e17976f995d8693fd90be49983cf9fa89ac1b11e
Ethan Lee <flibitijibibo@gmail.com>
2020-04-01 12:11:26 -0400

    One more flibitChange in stb_image_write

--- src/stb_image_write.h
+++ src/stb_image_write.h
@@ -188,7 +188,9 @@ typedef void stbi_write_func(void *context, void *data, int size);
 STBIWDEF int stbi_write_png_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const void  *data, int stride_in_bytes);
 STBIWDEF int stbi_write_bmp_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const void  *data);
 STBIWDEF int stbi_write_tga_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const void  *data);
+#if 0 /* flibitChange! */
 STBIWDEF int stbi_write_hdr_to_func(stbi_write_func *func, void *context, int w, int h, int comp, const float *data);
+#endif /* 0, flibitChange! */
 STBIWDEF int stbi_write_jpg_to_func(stbi_write_func *func, void *context, int x, int y, int comp, const void  *data, int quality);
 
 STBIWDEF void stbi_flip_vertically_on_write(int flip_boolean);
