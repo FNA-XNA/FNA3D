ca72557295236cce938f8240a5ee4fdd35ea80aa
Ethan Lee <flibitijibibo@gmail.com>
2020-05-01 10:17:01 -0400

    Bump version to match latest FNA

--- include/FNA3D.h
+++ include/FNA3D.h
@@ -455,7 +455,7 @@ typedef struct FNA3D_RenderTargetBinding
 
 #define FNA3D_ABI_VERSION	 0
 #define FNA3D_MAJOR_VERSION	20
-#define FNA3D_MINOR_VERSION	 4
+#define FNA3D_MINOR_VERSION	 5
 #define FNA3D_PATCH_VERSION	 0
 
 #define FNA3D_COMPILED_VERSION ( \
