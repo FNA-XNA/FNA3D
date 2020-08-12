4a7d3ad00eede8eb6cc841f536ace4fd5e8a209d
cosmonaut <evan@moonside.games>
2020-08-11 21:09:28 -0700

    fix hmgeti check

--- src/FNA3D_Driver_Vulkan.c
+++ src/FNA3D_Driver_Vulkan.c
@@ -2787,7 +2787,7 @@ static void VULKAN_INTERNAL_UpdateDescriptorSets(VulkanRenderer *renderer)
 		{
 			hash = VULKAN_INTERNAL_HashVertexSamplerDescriptorSetData(&renderer->vertexSamplerDescriptorSetDatas[i][j], i);
 
-			if (hmgeti(renderer->vertexSamplerDescriptorSetDataHashMap[i], hash) == -1)
+			if (hmgeti(renderer->vertexSamplerDescriptorSetDataHashMap[i], hash) < 0)
 			{
 				vertexSamplerDescriptorSetStructure = (VertexSamplerDescriptorSetDataHashMap) { hash, NULL_DESC_SET, renderer->vertexSamplerDescriptorSetDatas[i][j] };
 				hmputs(renderer->vertexSamplerDescriptorSetDataHashMap[i], vertexSamplerDescriptorSetStructure);
@@ -2804,7 +2804,7 @@ static void VULKAN_INTERNAL_UpdateDescriptorSets(VulkanRenderer *renderer)
 		{
 			hash = VULKAN_INTERNAL_HashFragSamplerDescriptorSetData(&renderer->fragSamplerDescriptorSetDatas[i][j], i);
 
-			if (hmgeti(renderer->fragSamplerDescriptorSetDataHashMap[i], hash) == -1)
+			if (hmgeti(renderer->fragSamplerDescriptorSetDataHashMap[i], hash) < 0)
 			{
 				fragSamplerDescriptorSetStructure = (FragSamplerDescriptorSetDataHashMap) { hash, NULL_DESC_SET, renderer->fragSamplerDescriptorSetDatas[i][j] };
 				hmputs(renderer->fragSamplerDescriptorSetDataHashMap[i], fragSamplerDescriptorSetStructure);
@@ -2819,7 +2819,7 @@ static void VULKAN_INTERNAL_UpdateDescriptorSets(VulkanRenderer *renderer)
 	{
 		hash = VULKAN_INTERNAL_HashVertexUniformDescriptorSetData(&renderer->vertexUniformDescriptorSetDatas[i]);
 
-		if (hmgeti(renderer->vertexUniformDescriptorSetDataHashMap, hash) == -1)
+		if (hmgeti(renderer->vertexUniformDescriptorSetDataHashMap, hash) < 0)
 		{
 			vertexUniformDescriptorSetStructure = (VertexUniformDescriptorSetDataHashMap) { hash, NULL_DESC_SET, renderer->vertexUniformDescriptorSetDatas[i] };
 			hmputs(renderer->vertexUniformDescriptorSetDataHashMap, vertexUniformDescriptorSetStructure);
@@ -2833,7 +2833,7 @@ static void VULKAN_INTERNAL_UpdateDescriptorSets(VulkanRenderer *renderer)
 	{
 		hash = VULKAN_INTERNAL_HashFragUniformDescriptorSetData(&renderer->fragUniformDescriptorSetDatas[i]);
 
-		if (hmgeti(renderer->fragUniformDescriptorSetDataHashMap, hash) == -1)
+		if (hmgeti(renderer->fragUniformDescriptorSetDataHashMap, hash) < 0)
 		{
 			fragUniformDescriptorSetStructure = (FragUniformDescriptorSetDataHashMap) { hash, NULL_DESC_SET, renderer->fragUniformDescriptorSetDatas[i] };
 			hmputs(renderer->fragUniformDescriptorSetDataHashMap, fragUniformDescriptorSetStructure);
