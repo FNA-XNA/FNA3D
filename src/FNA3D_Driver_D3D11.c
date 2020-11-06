04f403eb9a08f46e39543888b2cf85733e50621e
Ethan Lee <flibitijibibo@gmail.com>
2020-11-06 11:12:26 -0500

    D3D11: Fallback for error codes with no message

--- src/FNA3D_Driver_D3D11.c
+++ src/FNA3D_Driver_D3D11.c
@@ -59,18 +59,18 @@
 #define ERROR_CHECK(msg) \
 	if (FAILED(res)) \
 	{ \
-		D3D11_INTERNAL_LogError("%s! Error Code: %s (0x%08X)", msg, res); \
+		D3D11_INTERNAL_LogError(msg, res); \
 	}
 #define ERROR_CHECK_RETURN(msg, ret) \
 	if (FAILED(res)) \
 	{ \
-		D3D11_INTERNAL_LogError("%s! Error Code: %s (0x%08X)", msg, res); \
+		D3D11_INTERNAL_LogError(msg, res); \
 		return ret; \
 	}
 #define ERROR_CHECK_UNLOCK_RETURN(msg, ret) \
 	if (FAILED(res)) \
 	{ \
-		D3D11_INTERNAL_LogError("%s! Error Code: %s (0x%08X)", msg, res); \
+		D3D11_INTERNAL_LogError(msg, res); \
 		SDL_UnlockMutex(renderer->ctxLock); \
 		return ret; \
 	}
@@ -487,8 +487,7 @@ static const char* FAUX_BLIT_PIXEL_SHADER =
 /* Helper Functions */
 
 static void D3D11_INTERNAL_LogError(
-	const char* fmt,
-	char* msg,
+	const char *msg,
 	HRESULT res
 ) {
 	#define MAX_ERROR_LEN 1024 /* FIXME: Arbitrary! */
@@ -507,6 +506,13 @@ static void D3D11_INTERNAL_LogError(
 		MAX_ERROR_LEN,
 		NULL
 	);
+
+	/* No message? Screw it, just post the code. */
+	if (dwChars == 0)
+	{
+		FNA3D_LogError("%s! Error Code: 0x%08X", msg, res);
+		return;
+	}
 
 	/* Ensure valid range */
 	dwChars = SDL_min(dwChars, MAX_ERROR_LEN);
@@ -527,7 +533,7 @@ static void D3D11_INTERNAL_LogError(
 	/* Ensure null-terminated string */
 	wszMsgBuff[dwChars] = '\0';
 
-	FNA3D_LogError(fmt, msg, wszMsgBuff, res);
+	FNA3D_LogError("%s! Error Code: %s (0x%08X)", msg, wszMsgBuff, res);
 }
 
 static inline uint32_t D3D11_INTERNAL_CalcSubresource(
