805dd7a8123608497a99a05508eb235b1accd56a
cosmonaut <evan@moonside.games>
2022-01-04 22:37:12 -0800

    set TRANSFER_DST bit

--- src/FNA3D_Driver_Vulkan.c
+++ src/FNA3D_Driver_Vulkan.c
@@ -4862,7 +4862,7 @@ static VulkanBuffer* VULKAN_INTERNAL_CreateBuffer(
 
 	buffer->size = size;
 	buffer->resourceAccessType = resourceAccessType;
-	buffer->usage = usage | VK_BUFFER_USAGE_TRANSFER_SRC_BIT;
+	buffer->usage = usage | VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
 	buffer->preferDeviceLocal = isDeviceLocal;
 	buffer->isStagingBuffer = isStagingBuffer;
 
