ae26f8856b426eb66f8354843330cb0b39b10b26
Ethan Lee <flibitijibibo@gmail.com>
2023-07-09 19:47:31 -0400

    FNA3D_Driver.h buildfixes for C++-based drivers

--- src/FNA3D_Driver.h
+++ src/FNA3D_Driver.h
@@ -36,11 +36,17 @@
 #define inline __inline
 #endif
 
+#ifdef __cplusplus
+#define FNA3D_SHAREDINTERNAL extern "C"
+#else
+#define FNA3D_SHAREDINTERNAL extern
+#endif
+
 /* Logging */
 
-extern void FNA3D_LogInfo(const char *fmt, ...);
-extern void FNA3D_LogWarn(const char *fmt, ...);
-extern void FNA3D_LogError(const char *fmt, ...);
+FNA3D_SHAREDINTERNAL void FNA3D_LogInfo(const char *fmt, ...);
+FNA3D_SHAREDINTERNAL void FNA3D_LogWarn(const char *fmt, ...);
+FNA3D_SHAREDINTERNAL void FNA3D_LogError(const char *fmt, ...);
 
 /* Internal Helper Utilities */
 
@@ -818,10 +824,10 @@ typedef struct FNA3D_Driver
 	);
 } FNA3D_Driver;
 
-extern FNA3D_Driver VulkanDriver;
-extern FNA3D_Driver D3D11Driver;
-extern FNA3D_Driver OpenGLDriver;
-extern FNA3D_Driver GNMXDriver;
+FNA3D_SHAREDINTERNAL FNA3D_Driver VulkanDriver;
+FNA3D_SHAREDINTERNAL FNA3D_Driver D3D11Driver;
+FNA3D_SHAREDINTERNAL FNA3D_Driver OpenGLDriver;
+FNA3D_SHAREDINTERNAL FNA3D_Driver GNMXDriver;
 
 #endif /* FNA3D_DRIVER_H */
 
