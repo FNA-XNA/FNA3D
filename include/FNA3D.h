2919c2bdb2dedea93537d8224cde2ebf9138d9a3
Ethan Lee <flibitijibibo@gmail.com>
2024-04-01 09:49:23 -0400

    24.04

--- include/FNA3D.h
+++ include/FNA3D.h
@@ -469,7 +469,7 @@ typedef struct FNA3D_RenderTargetBinding
 
 #define FNA3D_ABI_VERSION	 0
 #define FNA3D_MAJOR_VERSION	24
-#define FNA3D_MINOR_VERSION	 3
+#define FNA3D_MINOR_VERSION	 4
 #define FNA3D_PATCH_VERSION	 0
 
 #define FNA3D_COMPILED_VERSION ( \
